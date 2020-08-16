4a879662000a11ea8a71438cbfd31ae8ea99cfe0
flxc3r <felix.carrier@gmail.com>
2020-08-16 17:09:10 -0400

    make User editable

--- my_webapp/my_webapp/user/forms.py
+++ my_webapp/my_webapp/user/forms.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """User forms."""
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import PasswordField, StringField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
@@ -42,4 +43,31 @@ class RegisterForm(FlaskForm):
         if user:
             self.email.errors.append("Email already registered")
             return False
+        return True
+
+
+class EditForm(FlaskForm):
+    """Edit form."""
+
+    email = StringField(
+        "Email", validators=[DataRequired(), Email(), Length(min=6, max=40)]
+    )
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(EditForm, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(EditForm, self).validate()
+        if not initial_validation:
+            return False
+        
+        # Unique email
+        user = User.query.filter_by(email=self.email.data).first()
+        if user and user!=current_user:
+            self.email.errors.append("Email already registered")
+            return False
+        
         return True
