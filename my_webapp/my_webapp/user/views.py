996ed3a463ca2a1bdb1722be1a9d9053699eea8f
flxc3r <felix.carrier@gmail.com>
2020-08-16 17:11:45 -0400

    make User editable (views)

--- my_webapp/my_webapp/user/views.py
+++ my_webapp/my_webapp/user/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """User views."""
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from my_webapp.user.forms import EditForm
+from my_webapp.utils import flash_errors
 
 blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
 
@@ -11,3 +13,19 @@ blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../s
 def members():
     """List members."""
     return render_template("users/members.html")
+
+
+@blueprint.route("/edit", methods=['GET', 'POST'])
+@login_required
+def edit():
+    """Edit members."""
+    form = EditForm(request.form)
+    if form.validate_on_submit():
+        current_user.update(
+            email=form.email.data,
+        )
+        flash("Changes saved", "success")
+        return redirect(url_for("user.members"))
+    else:
+        flash_errors(form)
+    return render_template("users/edit.html", form=form)
